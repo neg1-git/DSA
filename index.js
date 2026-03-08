424a35d8b3c34ce19e36776ee468c9be13b021c8
neg1-git <sahilnegi.bt23mech@pec.edu.in>
2026-03-08 16:36:09 +0530

    solved 3 sum

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-let numbers = [1,2,3,4] 
-let target = 3
+let nums = [-2,0,0,2,2] 
+let numbers= nums.sort((a,b)=>a-b)
+let output=[]
 
-let l=0
+for(let t=0 ; t<numbers.length ;t++){
+let target=-numbers[t]
+let l=t+1
 let r=numbers.length-1
-while(l<=r){
+if(numbers[t] === numbers[t-1]){continue}
+while(l<r){
   let sum=numbers[l]+numbers[r]
   if(sum>target){
     r--
@@ -12,10 +16,19 @@ while(l<=r){
     l++
   }
   if(sum===target){
-    console.log(l+1,r+1)
-    return
+    output.push([numbers[l],numbers[r],numbers[t]])
+    l++
+    r--
+    while (l < r && numbers[l] === numbers[l - 1]) {
+      l++;
+    }
   }
 }
+}
+
+console.log(output)
+
+
 
 
 
