a4ec76ab045b33d63af54646d4d08a77e4eb5a75
neg1-git <sahilnegi.bt23mech@pec.edu.in>
2026-03-24 23:07:44 +0530

    solved a new quest

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-let piles = [1,4,3,2]
-let h = 9
+let nums = [3,4,5,6,1,2]
 
-let output=Infinity;
-let l=1
-let r=Math.max(...piles)
-while(l<=r){
-  let k = Math.floor((l+r)/2)
-  let b=0;
-  console.log("rate is:",k)
-  for (let i = 0; i < piles.length; i++) {
+let l=0;
+let r=nums.length-1;
 
-    let a = Math.ceil(piles[i]/k)
-    b=a+b
-    
+while(l<r){
+  let m=Math.floor((l+r)/2);
+
+  if(nums[m]>nums[r]){
+    l=m+1
+  }else{
+    r=m
   }
-  console.log(b);
-  if(b<=h){
-    output=k
-    r=k-1
-  }
-  else{
-    l=k+1
-  }
+console.log(r)
 }
-  console.log(output)
 
