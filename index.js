9d30dfc7b3fea3121a2fdcec229a7eb032d40437
neg1-git <sahilnegi.bt23mech@pec.edu.in>
2026-03-26 02:13:25 +0530

    solved a question

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let nums = [3,4,5,6,1,2]
+let nums = [5,1,2,3,4]
+let target = 1
+
 
 let l=0;
 let r=nums.length-1;
@@ -6,11 +8,19 @@ let r=nums.length-1;
 while(l<r){
   let m=Math.floor((l+r)/2);
 
-  if(nums[m]>nums[r]){
-    l=m+1
+  if(nums[m]>=nums[l]){
+    if(nums[m]>=target && target>nums[l]){
+      r=m
+    }else{
+      l=m+1
+    }
   }else{
-    r=m
+    if(target>nums[m] && target<=nums[r]){
+      l=m+1
+    }else{
+      r=m
+    }
   }
-console.log(r)
+console.log(m)
 }
 
