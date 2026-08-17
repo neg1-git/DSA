08dad2b73e14ade6bb9273fd9208086b46430c25
neg1-git <sahilnegi.bt23mech@pec.edu.in>
2026-08-17 18:09:42 +0530

    solved a question

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hi')
+s = "[(])"
+
+const obj={
+    ")":"(",
+    "}":"{",
+    "]":"["
+}
+let stack=[]
+
+for(const i of s){
+    if(i in obj && stack[stack.length-1]===obj[i]){
+        stack.pop(i)
+    }else{
+    stack.push(i)}
+}
+if(stack.length===0){console.log(true)}
+else{console.log(false)}
+console.log(stack)
