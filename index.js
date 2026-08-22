2427a34a4f135c4c27470a73f738c54b29761836
neg1-git <sahilnegi.bt23mech@pec.edu.in>
2026-08-22 19:21:28 +0530

    solved a question

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 class Solution {
     /**
-     * @param {number} target
-     * @param {number[]} position
-     * @param {number[]} speed
-     * @return {number}
+     * @param {ListNode} l1
+     * @param {ListNode} l2
+     * @return {ListNode}
      */
-    carFleet(target, position, speed) {
-        // 1. Glue the data together so we don't lose track of which speed belongs to which car
-        let cars = [];
-        for (let i = 0; i < position.length; i++) {
-            cars.push({ pos: position[i], spd: speed[i] });
-        }
+    addTwoNumbers(l1, l2) {
+        let curr1=l1;
+        let curr2=l2;
+        let output= new ListNode(0);
+        let pointer=output;
+        let a=0
+        while(curr1!==null||curr2!==null||a==1){
 
-        // 2. Sort cars starting from the finish line (closest to target first)
-        cars.sort((a, b) => b.pos - a.pos);
+            if(curr1===null && curr2===null) a = a;
+            else if(curr1===null) a = a + curr2.val;
+            else if(curr2===null) a = a + curr1.val;
+            else {a = a + curr1.val + curr2.val;}
 
-        // The stack will hold the arrival times of the fleet leaders
-        let stack = [];
-
-        // 3. Process each car one by one
-        for (let i = 0; i < cars.length; i++) {
-            // Formula: Time = Distance / Speed
-            let time = (target - cars[i].pos) / cars[i].spd;
-            
-            // If the stack is empty, this is the first car (closest to target), so it's a leader.
-            // If this car takes MORE time than the leader ahead of it, it is too slow to catch up. 
-            // Therefore, it becomes the leader of a brand new fleet.
-            if (stack.length === 0 || time > stack[stack.length - 1]) {
-                stack.push(time);
+            if(a>9){
+                a=a-10;
+                let dummy = new ListNode(a);
+                pointer.next=dummy;
+                a=1
+            }else{
+                let dummy = new ListNode(a);
+                pointer.next=dummy;
+                a=0
             }
-            // (If the time was LESS than or EQUAL to the leader, it crashed into them. 
-            // We literally do nothing, discarding its time because it is now part of the leader's fleet).
+            if (curr1 !== null) {
+                curr1 = curr1.next;
+            }
+            if (curr2 !== null) {
+                curr2 = curr2.next;
+            }
+            pointer=pointer.next
         }
-
-        // The number of leaders in the stack is exactly the number of fleets
-        return stack.length;
+        return output.next
     }
 }
